b89fd6b1b7dc3f06e831bb5277dfd7d2f88322d7
Michael Shipley Barakat <michaebkats@gmail.com>
2016-08-06 13:17:04 -0700

    navigation updated

--- js/app.js
+++ js/app.js
@@ -4,3 +4,23 @@ $(document).ready(function() {
     $(this).addClass('active');
   });
 });
+
+var window = document.getElementById('window');
+
+function scrollReader (event){
+  var scrollAmount = $(window).scrollTop();
+  console.log(scrollAmount);
+
+  var navigation = document.getElementById('navbar');
+  if (scrollAmount >= 193){
+    var navigation = document.getElementById('navigation');
+    navigation.className = 'navigation fixed';
+  }
+
+  if (scrollAmount <= 192){
+    var navigation = document.getElementById('navigation');
+    navigation.className = 'navigation';
+  }
+};
+
+window.addEventListener('scroll', scrollReader);
