1b58e32ed6e38f917cbbf33babab87e2c754a48c
Michael Shipley Barakat <michaebkats@gmail.com>
2016-08-06 23:31:37 -0700

    iconography updates with animation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function() {
   $('a.menuButton').click(function() {
     $('a.menuButton.active').removeClass('active');
@@ -23,4 +25,28 @@ function scrollReader (event){
   }
 };
 
+
+// function heartPump (){
+// 	var heartOne = document.getElementById('heartOne');
+// 	var heartTwo = document.getElementById('heartTwo');
+// 	var heartStyles = heartOne.currentStyle || getComputedStyle(heartOne, null);
+//
+//   if (heartOne.className === 'center icon on'){
+//     setTimeout(
+// 			function(){
+// 			  heartOne.className = 'center icon off';
+// 			  heartTwo.className = 'center icon on';
+// 			}, 500);
+//   }
+//
+//   if (heartTwo.className === 'center icon on'){
+//     setTimeout(
+// 			function(){
+// 		  heartTwo.className = 'center icon off';
+// 		  heartOne.className = 'center icon on';
+// 		}, 500);
+//   }
+// }
+
+//window.setInterval(heartPump, 500);
 window.addEventListener('scroll', scrollReader);
